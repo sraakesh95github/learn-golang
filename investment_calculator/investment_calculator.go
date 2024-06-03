f705d5a62afebce751b128e07139aca0906f179a
sraakesh95github <raakesh95jobs@gmail.com>
2024-06-03 13:46:18 -0400

    Added example 2 for tax and profit calculation

---
+++ investment_calculator/investment_calculator.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	const inflationRate float64 = 6.5
+	years, expectedReturnRate := 10.0, 5.5
+	var investmentAmount float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+}
